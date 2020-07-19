c432799d884f0d35d536ccae21142efac2d84ce1
Harry Chen <i@harrychen.xyz>
2020-07-20 01:22:08 +0800

    Fix space in cmake, extract performance results
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -v
-rm -rf $3
+rm -rf "$3"
 if [ -e "$2" ]; then
-    qemu-arm $1 < $2 > $3
+    qemu-arm "$1" < "$2" > "$3"
 else
-    qemu-arm $1 > $3
+    qemu-arm "$1" > "$3"
 fi
 res=$?
 if [ -z "$(tail -c 1 "$3")" ]
 then
     # newline at eof
-    echo "${res}" >> $3
+    echo "${res}" >> "$3"
 else
     # no newline at eof
-    echo -n "\n${res}" >> $3
+    echo -n "\n${res}" >> "$3"
 fi
-diff -w -B -u $3 $4
+diff -w -B -u "$3" "$4"
