991c307f6bccb48b491c8c9c6392b501bb606f91
Harry Chen <i@harrychen.xyz>
2020-07-21 21:50:24 +0800

    Add comparison to python script
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- extract_result.py
+++ extract_result.py
@@ -2,16 +2,31 @@
 
 import sys
 import re
+from tabulate import tabulate
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         content = f.read()
 
-    pattern = re.compile(r'Test: check_run_llvm_(.*)[\s\S]*?TOTAL: (.*)')
+    pattern = re.compile(r'Test: check_run_(llvm|tc)_(.*)[\s\S]*?TOTAL: (\d+)H-(\d+)M-(\d+)S-(\d+)us')
+
+    results = {}
 
     try:
-        for name, time in pattern.findall(content):
-            print(f'{name}: {time}')
+        for compiler, name, h, m, s, us in pattern.findall(content):
+            h, m, s, us = int(h), int(m), int(s), int(us)
+            if name not in results:
+                results[name] = {}
+            results[name][compiler] = ((h * 60 + m) * 60 + s) * 1e6 + us
     except:
         pass
 
+    data = []
+
+    for case, time in results.items():
+        time_llvm = time['llvm']
+        time_tc = time['tc'] if 'tc' in time else float('inf')
+        ratio = f'+{time_tc * 100 / time_llvm:0.2f}%' if time_llvm != 0 else 'NaN'
+        data.append([case, time_llvm, time_tc, ratio])
+
+    print(tabulate(data, headers=["Case", "LLVM", "TC", "Ratio"]))
