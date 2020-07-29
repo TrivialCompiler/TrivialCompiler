0b0dcc92258335078eb67de83c54eeee167775df
Harry Chen <i@harrychen.xyz>
2020-07-29 18:22:10 +0800

    Add timeout to qemu-arm
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- test.sh
+++ test.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
+
+QEMU="timeout -v 60 qemu-arm"
+
 set -v
 rm -rf "$3"
+
+# run program with QEMU
 if [ -e "$2" ]; then
-    qemu-arm "$1" < "$2" > "$3"
+    $QEMU "$1" < "$2" > "$3"
 else
-    qemu-arm "$1" > "$3"
+    $QEMU "$1" > "$3"
 fi
+
 res=$?
+
 if [ -z "$(tail -c 1 "$3")" ]
 then
     # newline at eof
@@ -15,4 +22,6 @@ else
     # no newline at eof
     echo -n "\n${res}" >> "$3"
 fi
+
 diff -w -B -u "$3" "$4"
+
