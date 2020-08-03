0c6cdde8c3538683808669d9191a0bbfbcf0f1ee
MashPlant <740678788@qq.com>
2020-08-04 01:20:41 +0800

    fix lca == nullptr case in gvn_gcm; give up placing cmp before branch automatically, instead do this manually

--- src/passes/ir/gvn_gcm.cpp
+++ src/passes/ir/gvn_gcm.cpp
@@ -1,5 +1,5 @@
 #include "gvn_gcm.hpp"
-
+#include "dce.hpp"
 #include "../../op.hpp"
 #include "cfg.hpp"
 
@@ -92,7 +92,8 @@ static Value *vn_of(VN &vn, Value *x) {
 // 故两个操作符相同时结果为Add，否则为Rsb; c为Add是C2前是正号，否则是负号
 static void try_fold_lhs(BinaryInst *x) {
   if (auto r = dyn_cast<ConstValue>(x->rhs.value)) {
-    if (auto l = dyn_cast<BinaryInst>(x->lhs.value)) {
+    // 只有当l仅被x使用时才做这个优化，不然也没什么意义，总是要做两次计算的
+    if (auto l = dyn_cast<BinaryInst>(x->lhs.value); l && l->uses.head == l->uses.tail) {
       if (auto lr = dyn_cast<ConstValue>(l->rhs.value)) {
         if (x->tag == Value::Tag::Sub) x->rhs.set(r = new ConstValue(-r->imm)), x->tag = Value::Tag::Add;
         if (l->tag == Value::Tag::Sub) l->rhs.set(lr = new ConstValue(-lr->imm)), l->tag = Value::Tag::Add;
@@ -176,36 +177,34 @@ static void schedule_late(std::unordered_set<Inst *> &vis, LoopInfo &info, Inst 
         }
         lca = lca ? find_lca(lca, use) : use;
       }
-      // 如果lca为nullptr，意味着uses为空，意味着这条指令可以删掉，但是这个pass不负责处理无用代码
-      if (lca) {
-        BasicBlock *best = lca;
-        u32 best_loop_depth = info.depth_of(best);
-        // 论文里是while (lca != i->bb)，但是我觉得放在x->bb也是可以的，所以改成了考虑完lca后再判断是否等于x->bb
-        while (true) {
-          u32 cur_loop_depth = info.depth_of(lca);
-          if (cur_loop_depth < best_loop_depth) {
-            best = lca;
-            best_loop_depth = cur_loop_depth;
-          }
-          if (lca == i->bb) break;
-          lca = lca->idom;
+      // lca为空即没有人使用它，这并不是什么有意义的情形，而且也没法知道这条指令该放在哪里了
+      // 因此在gvn_gcm pass前需要先运行一遍dce pass，保证没有这种情形
+      assert(lca != nullptr);
+      BasicBlock *best = lca;
+      u32 best_loop_depth = info.depth_of(best);
+      // 论文里是while (lca != i->bb)，但是我觉得放在x->bb也是可以的，所以改成了考虑完lca后再判断是否等于x->bb
+      while (true) {
+        u32 cur_loop_depth = info.depth_of(lca);
+        if (cur_loop_depth < best_loop_depth) {
+          best = lca;
+          best_loop_depth = cur_loop_depth;
         }
-        transfer_inst(i, best);
-        for (Use *u = i->uses.head; u; u = u->next) {
-          // 如果use是PhiInst，一定不用调整位置，因为即使这条指令在bb的最后也能满足这个use
-          if (!isa<PhiInst>(u->user) && !isa<MemPhiInst>(u->user) && u->user->bb == i->bb) {
-            // 只有当x和u->user在同一个bb，且前者在后者后面时，把它移动到后者的前面一条指令的位置
-            // 这里需要判断链表中节点的相对位置，理论上是可以把它卡到非常耗时的，但是应该不会有这么无聊的测例
-            for (Inst *j = u->user; j; j = j->next) {
-              if (i == j) {
-                i->bb->insts.remove(i);
-                i->bb->insts.insertBefore(i, u->user);
-                break;
-              }
+        if (lca == i->bb) break;
+        lca = lca->idom;
+      }
+      transfer_inst(i, best);
+      for (Inst *j = best->insts.head; j; j = j->next) {
+        if (!isa<PhiInst>(j)) {
+          for (Use *u = i->uses.head; u; u = u->next) {
+            if (u->user == j) {
+              best->insts.remove(i);
+              best->insts.insertBefore(i, j);
+              goto out;
             }
           }
         }
       }
+      out:;
     }
   }
 }
@@ -266,22 +265,28 @@ void gvn_gcm(IrFunc *f) {
       i = next;
     }
   }
+  // 不是很方便在pass manager里表示这种关系，就手动调用了
+  dce(f);
   // 阶段2，gcm
   LoopInfo info = compute_loop_info(f);
-  std::unordered_set<Inst *> vis;
-  for (BasicBlock *bb = entry; bb; bb = bb->next) {
-    for (Inst *i = bb->insts.head; i;) {
-      Inst *next = i->next;
-      schedule_early(vis, entry, i);
-      i = next;
-    }
-  }
-  vis.clear();
   std::vector<Inst *> insts;
   for (BasicBlock *bb = entry; bb; bb = bb->next) {
-    insts.clear();
-    // 用Inst *next = i->next; ... i = next;也不能保证遍历所有指令，可能后面的一些指令被移走了，所以这里先把所有指令保存下来
     for (Inst *i = bb->insts.head; i; i = i->next) insts.push_back(i);
-    for (Inst *i : insts) schedule_late(vis, info, i);
+  }
+  std::unordered_set<Inst *> vis;
+  for (Inst *i : insts) schedule_early(vis, entry, i);
+  vis.clear();
+  for (Inst *i : insts) schedule_late(vis, info, i);
+  // fixme: 其实本质上这个算法是没办法决定一个bb内的任何顺序的，需要别的调度策略，这里简单做一个，激活cmp + branch的优化
+  for (BasicBlock *bb = entry; bb; bb = bb->next) {
+    if (auto x = dyn_cast<BranchInst>(bb->insts.tail)) {
+      if (x->cond.value->tag >= Value::Tag::Lt && x->cond.value->tag <= Value::Tag::Ne) {
+        auto c = static_cast<Inst *>(x->cond.value);
+        if (c->bb == bb && c->uses.head == c->uses.tail) { // 要求只被这个branch使用
+          bb->insts.remove(c);
+          bb->insts.insertBefore(c, x);
+        }
+      }
+    }
   }
 }
