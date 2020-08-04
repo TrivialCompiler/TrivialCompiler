d745b4cdd628079bb73cb3107f343fd1046702f1
Jiajie Chen <jiegec@qq.com>
2020-08-04 09:43:15 +0800

    Compare against clang

--- extract_result.py
+++ extract_result.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         content = f.read()
 
-    pattern = re.compile(r'Test: check_run_(llvm|tc|gcc)_(.*)[\s\S]*?TOTAL: (\d+)H-(\d+)M-(\d+)S-(\d+)us')
+    pattern = re.compile(r'Test: check_run_(llvm|tc|gcc|clang)_(.*)[\s\S]*?TOTAL: (\d+)H-(\d+)M-(\d+)S-(\d+)us')
 
     results = {}
 
@@ -29,9 +29,11 @@ if __name__ == '__main__':
         nan = float('nan')
         time_llvm = time['llvm'] if 'llvm' in time else nan
         time_gcc = time['gcc'] if 'gcc' in time else nan
+        time_clang = time['clang'] if 'clang' in time else nan
         time_tc = time['tc'] if 'tc' in time else nan
         ratio_llvm = f'{time_tc * 100 / time_llvm:0.2f}%' if time_llvm != 0 else 'N/A'
         ratio_gcc = f'{time_tc * 100 / time_gcc:0.2f}%' if time_gcc != 0 else 'N/A'
-        data.append([case, time_llvm / 1e6, time_gcc / 1e6, time_tc / 1e6, ratio_llvm, ratio_gcc])
+        ratio_clang = f'{time_tc * 100 / time_clang:0.2f}%' if time_clang != 0 else 'N/A'
+        data.append([case, time_llvm / 1e6, time_gcc / 1e6, time_clang / 1e6, time_tc / 1e6, ratio_llvm, ratio_gcc, ratio_clang])
 
-    print(tabulate(data, headers=["Case", "LLVM", "GCC", "TC", "Ratio(LLVM)", "Ratio(GCC)"]))
+    print(tabulate(data, headers=["Case", "LLVM", "GCC", "Clang", "TC", "Ratio(LLVM)", "Ratio(GCC)", "Ratio(Clang)"]))
