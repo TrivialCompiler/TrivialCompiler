f0bc3c7d26072af3c8a8d3d5269f2aab61bf2726
Harry Chen <i@harrychen.xyz>
2020-08-08 23:36:59 +0800

    Inline only global scalar constants
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/passes/ir/mark_global_const.cpp
+++ src/passes/ir/mark_global_const.cpp
@@ -22,6 +22,8 @@ static bool has_store(Value *v) {
 void mark_global_const(IrProgram *p) {
   for (Decl *d : p->glob_decl) {
     if (!d->is_const && !has_store(d->value)) {
+      // NOTE_OPT: we choose not to inline global arrays (which causes bad performance on bitset)
+      if (!d->dims.empty()) continue;
       d->is_const = true;
       auto global_const = "Marking global variable '" + std::string(d->name) + "' as const";
       dbg(global_const);
