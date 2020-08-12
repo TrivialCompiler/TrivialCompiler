b06a920ce418d89b00b6bed57e971a5feb49fbc7
Jiajie Chen <jiegec@qq.com>
2020-08-12 08:13:47 +0800

    Use loop instead of next_inst in outer loop

--- src/passes/ir/extract_stack_array.cpp
+++ src/passes/ir/extract_stack_array.cpp
@@ -8,8 +8,6 @@
 void extract_stack_array(IrProgram *p) {
   for (auto f = p->func.head; f; f = f->next) {
     if (f->builtin) continue;
-    BasicBlock *bb = f->bb.head;
-    Inst *inst = bb->insts.head;
     auto next_inst = [](Inst *inst, BasicBlock *&bb) -> Inst * {
       if (inst && inst->next) {
         return inst->next;
@@ -24,86 +22,88 @@ void extract_stack_array(IrProgram *p) {
       }
     };
     // iterate through all instructions in a BB
-    for (; inst; inst = next_inst(inst, bb)) {
-      // check each AllocaInst
-      if (auto alloc = dyn_cast<AllocaInst>(inst)) {
-        bool can_make_global = true;
-        auto size = alloc->sym->dims[0]->result;
-        auto buffer = new int[size]();  // auto initialized to 0
-        std::unordered_set<StoreInst *> stores, met_stores;
-        CallInst *memset = nullptr;
-        for (auto use = alloc->uses.head; use; use = use->next) {
-          if (auto store = dyn_cast<StoreInst>(use->user)) {
-            if (auto data = dyn_cast<ConstValue>(store->data.value), index = dyn_cast<ConstValue>(store->index.value);
-                data && index) {
-              buffer[index->imm] = data->imm;
-              stores.insert(store);
-            } else {
+    for (auto bb = f->bb.head; bb; bb = bb->next) {
+      for (auto inst = bb->insts.head; inst; inst = inst->next) {
+        // check each AllocaInst
+        if (auto alloc = dyn_cast<AllocaInst>(inst)) {
+          bool can_make_global = true;
+          auto size = alloc->sym->dims[0]->result;
+          auto buffer = new int[size]();  // auto initialized to 0
+          std::unordered_set<StoreInst *> stores, met_stores;
+          CallInst *memset = nullptr;
+          for (auto use = alloc->uses.head; use; use = use->next) {
+            if (auto store = dyn_cast<StoreInst>(use->user)) {
+              if (auto data = dyn_cast<ConstValue>(store->data.value), index = dyn_cast<ConstValue>(store->index.value);
+                  data && index) {
+                buffer[index->imm] = data->imm;
+                stores.insert(store);
+              } else {
+                can_make_global = false;
+                break;
+              }
+            } else if (isa<GetElementPtrInst>(use->user)) {
+              // used for function calling
               can_make_global = false;
               break;
             }
-          } else if (isa<GetElementPtrInst>(use->user)) {
-            // used for function calling
-            can_make_global = false;
-            break;
           }
-        }
-        if (can_make_global) {
-          BasicBlock *bb_check = alloc->bb;
-          Inst *inst_check;
-          // iterate from next instruction, until the first use
-          for (inst_check = next_inst(alloc, bb_check); inst_check; inst_check = next_inst(inst_check, bb_check)) {
-            if (auto store = dyn_cast<StoreInst>(inst_check)) {
-              if (store->arr.value == alloc) {
-                met_stores.insert(store);
-              }
-            } else if (auto load = dyn_cast<LoadInst>(inst_check)) {
-              if (load->arr.value == alloc) break;
-            } else if (auto ptr = dyn_cast<GetElementPtrInst>(inst_check)) {
-              if (ptr->arr.value == alloc) break;
-            } else if (auto call = dyn_cast<CallInst>(inst_check)) {
-              for (auto &arg : call->args) {
-                // can only be memset, and will only appear once
-                if (arg.value == alloc) {
-                  assert(memset == nullptr && call->func == Func::BUILTIN[8].val);
-                  memset = call;
+          if (can_make_global) {
+            BasicBlock *bb_check = alloc->bb;
+            Inst *inst_check;
+            // iterate from next instruction, until the first use
+            for (inst_check = next_inst(alloc, bb_check); inst_check; inst_check = next_inst(inst_check, bb_check)) {
+              if (auto store = dyn_cast<StoreInst>(inst_check)) {
+                if (store->arr.value == alloc) {
+                  met_stores.insert(store);
                 }
+              } else if (auto load = dyn_cast<LoadInst>(inst_check)) {
+                if (load->arr.value == alloc) break;
+              } else if (auto ptr = dyn_cast<GetElementPtrInst>(inst_check)) {
+                if (ptr->arr.value == alloc) break;
+              } else if (auto call = dyn_cast<CallInst>(inst_check)) {
+                for (auto &arg : call->args) {
+                  // can only be memset, and will only appear once
+                  if (arg.value == alloc) {
+                    assert(memset == nullptr && call->func == Func::BUILTIN[8].val);
+                    memset = call;
+                  }
+                }
+              } else if (isa<BranchInst>(inst_check)) {
+                break;
               }
-            } else if (isa<BranchInst>(inst_check)) {
-              break;
+            }
+            if (met_stores == stores) {
+              // all stores are visited, good!
+              // extract this param to global
+              std::vector<Expr *> init;
+              init.reserve(size);
+              for (int i = 0; i < size; ++i) {
+                init.push_back(buffer[i] == 0 ? &IntConst::ZERO : new IntConst{Expr::Tag::IntConst, buffer[i]});
+              }
+              auto name =
+                  new std::string("__extracted_" + std::string(f->func->name) + "_" + std::string(alloc->sym->name));
+              auto extract_array = "Extract local array " + std::string(alloc->sym->name) + " to global " + *name;
+              dbg(extract_array);
+              auto extracted_decl =
+                  new Decl{true, true, true, {name->c_str(), name->length()}, alloc->sym->dims, {nullptr}, init};
+              extracted_decl->value = new GlobalRef(extracted_decl);
+              p->glob_decl.push_back(extracted_decl);
+              // remove memset
+              if (memset) {
+                memset->bb->insts.remove(memset);
+                delete memset;
+              }
+              // replace all use to global ref
+              alloc->replaceAllUseWith(extracted_decl->value);
+              // remove all stores
+              for (auto &s : stores) {
+                s->bb->insts.remove(s);
+                delete s;
+              }
             }
           }
-          if (met_stores == stores) {
-            // all stores are visited, good!
-            // extract this param to global
-            std::vector<Expr *> init;
-            init.reserve(size);
-            for (int i = 0; i < size; ++i) {
-              init.push_back(buffer[i] == 0 ? &IntConst::ZERO : new IntConst{Expr::Tag::IntConst, buffer[i]});
-            }
-            auto name =
-                new std::string("__extracted_" + std::string(f->func->name) + "_" + std::string(alloc->sym->name));
-            auto extract_array = "Extract local array " + std::string(alloc->sym->name) + " to global " + *name;
-            dbg(extract_array);
-            auto extracted_decl =
-                new Decl{true, true, true, {name->c_str(), name->length()}, alloc->sym->dims, {nullptr}, init};
-            extracted_decl->value = new GlobalRef(extracted_decl);
-            p->glob_decl.push_back(extracted_decl);
-            // remove memset
-            if (memset) {
-              memset->bb->insts.remove(memset);
-              delete memset;
-            }
-            // replace all use to global ref
-            alloc->replaceAllUseWith(extracted_decl->value);
-            // remove all stores
-            for (auto &s : stores) {
-              s->bb->insts.remove(s);
-              delete s;
-            }
-          }
+          delete[] buffer;
         }
-        delete[] buffer;
       }
     }
   }
